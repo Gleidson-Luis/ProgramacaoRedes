bd6bae15a973bbfa804606030a3b22825c2b7a3c
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2025-05-16 21:24:18 -0300

    Resolução da questao 9

--- Lista1/Q9.py
+++ Lista1/Q9.py
@@ -1,6 +1,6 @@
 # Solicitar ao usuário o endereço MAC de um dispositivo
 
-mac = input("Digite o endereço MAC de um dispositivo (formato XX:XX:XX:XX:XX): ")
+mac = input("Digite o endereço MAC de um dispositivo: ")
 
 if mac.startswith("00:1A:2B"):
     print("O dispositivo pertece ao fabricante X")
