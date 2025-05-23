8782c1b15cf306057b718d4f1d364dc1c5d547b1
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2025-05-23 20:01:41 -0300

    Criando Pasta

--- Lista1/Q10.py
+++ Lista1/Q10.py
@@ -1,8 +1,10 @@
+# Pergunte o nome de uma pasta e criar uma pasta no sistema, usando módulo os
+
 import os
 
 nome_pasta = input("Digite o nome da paste que deseja criar: ")
-# Obtém o caminho completo onde a pasta será criada (no diretório atual)
-caminho_pasta = os.path.join(os.getcwd(), nome_pasta)
+
+caminho_pasta = f"C:\PastaTeste\{nome_pasta}"
 
 # Verifica se a pasta já existe e a cria se não existir
 if not os.path.exists(caminho_pasta):
