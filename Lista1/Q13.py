d1640aaeff0b15db81f88ddd27a17d145bb01eca
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2025-05-23 21:49:27 -0300

    Salvar site html

--- Lista1/Q13.py
+++ Lista1/Q13.py
@@ -4,7 +4,8 @@ import requests
 
 site = input("Digite o endereço do site: ")
 conteudo = requests.get(site)
-arquivo = open("site.html", "w")
-arquivo.write(conteudo.text)
-print("O conteúdo HTML foi salvo com sucesso em 'site.html'.")
-arquivo.close()
+
+with open("index.html","w", encoding = conteudo.apparent_encoding) as arquivo:
+    arquivo.write(conteudo.text)
+    print("O conteúdo HTML foi salvo com sucesso em 'site.html'.")
+    arquivo.close()
