33d7da14735bdc2bcd1eb85c9cf1820a98e3c0cd
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2025-06-27 19:28:06 -0300

    Q2-2

--- Lista1/Q20.py
+++ Lista1/Q20.py
@@ -4,6 +4,6 @@ arquivo = open("log.txt","w")
 
 while True:
     #print(time.strftime("%a, %d, %b %Y %H:%M:%S", time.gmtime()))
-    arquivo.writelines(time.strftime("%H:%M:%S\n", time.gmtime()))
+    arquivo.writelines(time.strftime("%H:%M:%S\n"), time.gmtime())
     time.sleep(5)
     
