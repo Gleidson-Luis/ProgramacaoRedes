62c977922e4d2c1ed4e3b0c910973746251ed4ae
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2025-06-27 19:27:40 -0300

    Q20

---
+++ Lista1/Q20.py
@@ -0,0 +1,9 @@
+import time
+
+arquivo = open("log.txt","w")
+
+while True:
+    #print(time.strftime("%a, %d, %b %Y %H:%M:%S", time.gmtime()))
+    arquivo.writelines(time.strftime("%H:%M:%S\n", time.gmtime()))
+    time.sleep(5)
+    
