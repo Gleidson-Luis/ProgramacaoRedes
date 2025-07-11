899192d5311ea22446e66a2749471a67f330eac4
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2025-07-11 20:09:32 -0300

    Q22

---
+++ Lista1/Q22.py
@@ -0,0 +1,14 @@
+
+import requests
+
+lista_sites = ["http://www.g1.com.br", "http://www.ifrn.edu.br", "http://www.uol.com.br"]
+
+for site in lista_sites:
+    try:
+        resposta = requests.get(site)
+        if resposta.status_code ==200:
+            print("Online")
+        else:
+            print("Offline")
+    except:
+        print("Erro")
