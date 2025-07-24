363c0718aab2c1bf9a0263487a1d1b19b276a69a
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2025-07-24 20:21:28 -0300

    Questao2

---
+++ Lista2/Q2.py
@@ -0,0 +1,7 @@
+# Percorrer arquivos de um diretorio e exibir somente arquivos com extensão .log
+
+import os
+
+caminho_diretorio = "C:\Users\gleid\OneDrive\Área de Trabalho\ProgramacaoRedes\quest2"
+arquivos = os.listdir(caminho_diretorio)
+
